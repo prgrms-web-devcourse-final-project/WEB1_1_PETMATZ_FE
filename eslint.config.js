395de5f13459a45aa3c27e6d68812b378e06e974
Kim-HyounSuk <kkoor586@naver.com>
2024-11-19 14:56:14 +0900

    🔧 prettier 플러그인 추가

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,7 @@ export default tseslint.config(
         plugins: {
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
+            prettier,
         },
         rules: {
             ...reactHooks.configs.recommended.rules,
