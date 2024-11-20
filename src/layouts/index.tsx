f1f6794f82f931cfa492d7ce596ecaf82c445b03
dalsu0222 <dalsu0222@sju.ac.kr>
2024-11-20 10:41:50 +0900

    💄 루트레이아웃 소폭 수정

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -8,7 +8,7 @@ interface RootLayoutProps {
 
 export default function RootLayout({ layout }: RootLayoutProps) {
     return (
-        <div className="min-h-screen relative max-w-[768px] bg-white text-secondary mx-auto flex flex-col justify-center px-1">
+        <div className="min-h-screen relative max-w-[768px] bg-white text-secondary mx-auto flex flex-col justify-center px-1 shadow-[0_7px_29px_0px_rgba(100,100,111,0.5)] overflow-hidden">
             {layout && <Header />}
             <main className="flex-1">
                 <Outlet />
