1025ec7195223db50d863ff157264fec1a15c5b6
dalsu0222 <dalsu0222@sju.ac.kr>
2024-11-20 11:50:34 +0900

    💄 overflow 수정

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -8,9 +8,9 @@ interface RootLayoutProps {
 
 export default function RootLayout({ layout }: RootLayoutProps) {
     return (
-        <div className="min-h-screen relative max-w-[768px] bg-white text-secondary mx-auto flex flex-col justify-center px-1 shadow-[0_7px_29px_0px_rgba(100,100,111,0.5)] overflow-hidden">
+        <div className="min-h-screen relative max-w-[768px] min-w-[360px] bg-white text-secondary mx-auto flex flex-col justify-center shadow-[0_7px_29px_0px_rgba(100,100,111,0.5)] overflow-hidden">
             {layout && <Header />}
-            <main className="flex-1">
+            <main className="flex-1 overflow-y-auto">
                 <Outlet />
             </main>
             {layout && <BottomNav />}
