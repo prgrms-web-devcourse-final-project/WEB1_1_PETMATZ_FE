b522f953f1c57c676f68c8622ee00625dc6cb373
ysmuei <ysmuei@naver.com>
2024-11-22 12:39:41 +0900

    💄 버튼 hover 색상 지정

--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -27,7 +27,7 @@ export default function Intro() {
             <section className="w-full max-w-[600px] px-6 py-2.5">
                 {currentSlide < totalSlides - 1 ? (
                     <button
-                        className="w-full text-body-l font-extrabold text-white py-3 mb-8 rounded-lg bg-point-500 active:bg-point-600"
+                        className="w-full text-body-l transition-colors font-extrabold text-white py-3 mb-8 rounded-lg bg-point-500 active:bg-point-600 hover:bg-point-600"
                         onClick={() =>
                             (
                                 document.querySelector(
@@ -40,16 +40,18 @@ export default function Intro() {
                     </button>
                 ) : (
                     <div className="flex flex-col gap-3 mb-4">
-                        <button
-                            onClick={() => navigate('/signup')}
-                            className="text-point-400 active:text-point-600 text-label-l sm:text-body-l font-semibold flex justify-center items-center gap-1 mb-2"
-                        >
-                            아직 회원이 아니신가요? <ArrowRight />
-                        </button>
+                        <div className="w-full flex justify-center items-center">
+                            <button
+                                onClick={() => navigate('/signup')}
+                                className="text-point-400 transition-colors active:text-point-600 hover:text-point-600 text-label-l sm:text-body-l font-semibold flex justify-center items-center gap-1 mb-2"
+                            >
+                                아직 회원이 아니신가요? <ArrowRight />
+                            </button>
+                        </div>
 
                         <div className="flex flex-row gap-2.5">
                             <button
-                                className="w-full flex justify-center items-center gap-3 text-point-600 text-body-l font-extrabold py-3.5 px-6 rounded-lg border-2 border-point-500 active:bg-point-200"
+                                className="w-full transition-colors flex justify-center items-center gap-3 text-point-600 text-body-l font-extrabold py-3.5 px-6 rounded-lg border-2 border-point-500 active:bg-point-200 hover:bg-point-200"
                                 onClick={() => navigate('/login')}
                             >
                                 <AtSign />
