1c489af1bbf1e7d8811eb5a2b66ad3e31298f3db
dalsu0222 <dalsu0222@sju.ac.kr>
2024-11-25 11:40:22 +0900

    ✨ 커스텀 toast 컴포넌트 작성

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,3 +3,4 @@ export { default as CustomInput } from './CustomInput';
 export { default as SelectBox } from './SelectBox';
 export { default as DogSelector } from './DogSelector';
 export { default as Loading } from './Loading';
+export { CustomToast, ToastAnchor, useCustomToast } from './CustomToast';
