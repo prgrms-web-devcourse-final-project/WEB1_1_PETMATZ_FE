480cb95aa0dfafc5b3bb9545c3ec49a309ab601e
dalsu0222 <dalsu0222@sju.ac.kr>
2024-11-26 11:30:30 +0900

    ✨ 컴포넌트 관련 변환 유틸 함수 작성

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
 export { default as utcToLocalTime } from './utcToLocalTime';
 export { default as utcToLocalDateTime } from './utcToLocalDateTime';
 export { formatDateToString, parseStringToDate } from './dateUtils';
+export {
+    convertArrayToCommaString,
+    convertCommaStringToArray,
+} from './signup/stringArrayConverter';
