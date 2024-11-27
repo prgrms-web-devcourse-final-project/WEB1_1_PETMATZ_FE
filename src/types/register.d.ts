f781efaad7ac4d03c5033bc31fbb8d4243aacb79
dalsu0222 <dalsu0222@sju.ac.kr>
2024-11-28 01:52:26 +0900

    🏷️ 동물등록조회 관련 type 생성

--- src/types/register.d.ts
+++ src/types/register.d.ts
@@ -1,3 +1,5 @@
+import { BaseApiResponse } from './baseResponse';
+
 export interface RegisterFormData {
     ownerName: string;
     registrationNumber: string;
@@ -16,6 +18,35 @@ export interface RegisterStep1Props {
     register: UseFormRegister<RegisterFormData>;
     watch: UseFormWatch<RegisterFormData>;
     errors: FieldErrors<RegisterFormData>;
-    getValue: UseFormGetValues<RegisterFormData>;
+    // getValue: UseFormGetValues<RegisterFormData>;
     setValue: UseFormSetValue<RegisterFormData>;
+}
+
+// API 요청 인터페이스
+export interface AnimalRegistrationParams {
+    dog_reg_no: string; // 동물 등록 번호 (15자리 숫자)
+    owner_nm: string; // 소유자 성명
+}
+
+// API 응답 인터페이스
+export interface Register {
+    dogNm: string;
+    dogRegNo: string;
+    kindNm: string;
+    neuterYn: string;
+    sexNm: string;
+    // profileImg?: string;
+}
+
+// API 전체 응답 인터페이스
+export interface AnimalRegistrationResponse {
+    response: {
+        header: {
+            resultCode: string;
+            resultMsg: string;
+        };
+        body: {
+            item?: AnimalRegistrationData;
+        };
+    };
 }
