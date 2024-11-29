f85f3eb9d86cce9bae3af3b8542f2b15214c773a
Kim-HyounSuk <kkoor586@naver.com>
2024-11-30 02:56:50 +0900

    ✨ 사용자 인증 컴포넌트 작성

---
+++ src/components/auth/AuthGard.tsx
@@ -0,0 +1,33 @@
+import { getMyProfileInfo } from '@/hooks/api/profile';
+import { useUserStore } from '@/stores';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+interface AuthenticationProps {
+    element: React.ReactNode;
+    redirectTo: string;
+}
+
+export default function AuthGuard({ element }: AuthenticationProps) {
+    const { user, setUser } = useUserStore();
+    const navigate = useNavigate();
+
+    const validationAuth = async () => {
+        const { ok, data } = await getMyProfileInfo();
+
+        if (!ok) {
+            navigate('/login');
+        } else {
+            const { id, accountId, nickname, isRegistered, region } = data;
+            setUser({ id, accountId, nickname, isRegistered, region });
+        }
+    };
+
+    useEffect(() => {
+        if (!user) {
+            validationAuth();
+        }
+    }, [user]);
+
+    return element;
+}
