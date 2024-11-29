c56e2f5824c06b35e1c5322e2a44ac539e27713e
Kim-HyounSuk <kkoor586@naver.com>
2024-11-29 23:40:18 +0900

    ♻️ 로그인 유저 기반 동작으로 수정

--- src/pages/Match.tsx
+++ src/pages/Match.tsx
@@ -1,22 +1,25 @@
 import ReactDOM from 'react-dom';
-import { useMatchStore } from '@/stores';
+import { useMatchStore, useUserStore } from '@/stores';
 import { AnimatePresence } from 'framer-motion';
 import { useEffect, useState } from 'react';
-import { IntroMatch, MatchCard } from '@/components/match';
+import { IntroMatch, MatchCard, NoMoreCard } from '@/components/match';
 import FetchMoreCard from '@/components/match/FetchMoreCard';
 
 export default function Match() {
-    const { matchList, removeMatch, fetchMatchList, setShowStamp } =
+    const { matchList, removeMatch, fetchMatchList, setShowStamp, isLastPage } =
         useMatchStore();
+    const { user } = useUserStore();
     const [isModalOpen, setIsModalOpen] = useState(true);
 
     // 카드 관련 이벤트 핸들러
-    const handleDragEnd = (id: string) => {
-        removeMatch(id);
-        setShowStamp(false);
+    const handleDragEnd = (otherId: number) => {
+        if (user) {
+            removeMatch(user.id, otherId);
+            setShowStamp(false);
+        }
     };
     const handleFetchMore = () => {
-        fetchMatchList();
+        if (user) fetchMatchList(user.id);
     };
     const handleOnClickBtn = () => {
         if (matchList.length > 0) setShowStamp(true);
@@ -29,17 +32,21 @@ export default function Match() {
     };
 
     useEffect(() => {
-        if (!(matchList.length > 0)) fetchMatchList();
+        if (!(matchList.length > 0) && user) fetchMatchList(user.id);
 
         const skip = localStorage.getItem('skipMatchIntro');
         if (skip === 'true') {
             setIsModalOpen(false);
         }
+
+        return () => {
+            useMatchStore.setState({ isLastPage: false });
+        };
     }, []);
 
     return (
         <>
-            <div className="h-screen flex flex-col items-center justify-center gap-[120px] bg-gray-100">
+            <div className="h-screen flex flex-col items-center justify-center bg-gray-100">
                 <div className="relative w-[240px] h-[400px] flex items-center justify-center">
                     <AnimatePresence>
                         {/* 가장 위 카드와 바로 아래 카드만 렌더링 */}
@@ -52,17 +59,17 @@ export default function Match() {
                                 onDragEnd={handleDragEnd}
                             />
                         ))}
-
                         {/* 모든 카드가 사라지면 새로운 데이터를 요청하는 카드 표시 */}
-                        {matchList.length === 0 && (
+                        {isLastPage && <NoMoreCard />}
+                        {matchList.length === 0 && !isLastPage && (
                             <FetchMoreCard onFetchMore={handleFetchMore} />
                         )}
                     </AnimatePresence>
                 </div>
                 <button
-                    className="btn-solid max-w-[240px]"
+                    className={`btn-solid max-w-[240px] ${(matchList.length === 0 || isLastPage) && 'active:scale-100'}`}
                     onClick={handleOnClickBtn}
-                    disabled={!(matchList.length > 0)}
+                    disabled={matchList.length === 0 || isLastPage}
                 >
                     우리 멍멍이 부탁하기
                 </button>
