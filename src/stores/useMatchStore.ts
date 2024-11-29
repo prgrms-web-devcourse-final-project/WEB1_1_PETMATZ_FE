15e2b9a2113ad1d4e9145ba39f77bbdd5638c5de
Kim-HyounSuk <kkoor586@naver.com>
2024-11-29 23:41:16 +0900

    매칭 Api 활용 로직 작성

--- src/stores/useMatchStore.ts
+++ src/stores/useMatchStore.ts
@@ -1,79 +1,79 @@
-import { Match } from '@/types/match';
+import { getMatchList, postMatchFail } from '@/hooks/api/match';
+import { IMatch } from '@/types/match';
 import { create } from 'zustand';
 
-export interface MatchWidthColor extends Match {
+export interface MatchWidthColor extends IMatch {
     color?: string;
 }
 
 interface MatchStore {
     matchList: MatchWidthColor[];
     showStamp: boolean;
-    removeMatch: (id: string) => void;
-    fetchMatchList: () => void;
+    removeMatch: (userId: number, otherId: number) => void;
+    fetchMatchList: (userId: number) => void;
     setShowStamp: (state: boolean) => void;
+    curPage: number;
+    totalPages: number;
+    isLastPage: boolean;
 }
 
-const useMatchStore = create<MatchStore>((set) => ({
+const useMatchStore = create<MatchStore>((set, get) => ({
     matchList: [],
     showStamp: false,
+    curPage: 1,
+    totalPages: 1,
+    isLastPage: false,
     setShowStamp: (state) => set(() => ({ showStamp: state })),
-    removeMatch: (id) =>
+    removeMatch: async (userId, otherId) => {
+        const { ok } = await postMatchFail({
+            userId,
+            targetUserId: otherId,
+        });
+
+        if (!ok) return;
+
         set((state) => ({
-            matchList: state.matchList.filter((match) => match.id !== id),
-        })),
-    fetchMatchList: () =>
-        set(() => ({
-            matchList: [
-                {
-                    id: '1',
-                    nickname: '테스트1',
-                    introduction: '애견인입니다.',
-                    recommendationCount: 101,
-                    careCompletionCount: 105,
-                    latitude: 37.55258760777118,
-                    longitude: 126.97441248152484,
-                },
-                {
-                    id: '2',
-                    nickname: '테스트2',
-                    introduction: '애견인입니다.',
-                    recommendationCount: 101,
-                    careCompletionCount: 105,
-                    latitude: 37.55258760777118,
-                    longitude: 126.97441248152484,
-                },
-                {
-                    id: '3',
-                    nickname: '테스트3',
-                    introduction: '애견인입니다.',
-                    recommendationCount: 101,
-                    careCompletionCount: 105,
-                    latitude: 37.55258760777118,
-                    longitude: 126.97441248152484,
-                },
-                {
-                    id: '4',
-                    nickname: '테스트4',
-                    introduction: '애견인입니다.',
-                    recommendationCount: 101,
-                    careCompletionCount: 105,
-                    latitude: 37.55258760777118,
-                    longitude: 126.97441248152484,
-                },
-                {
-                    id: '5',
-                    nickname: '테스트5',
-                    introduction: '애견인입니다.',
-                    recommendationCount: 101,
-                    careCompletionCount: 105,
-                    latitude: 37.55258760777118,
-                    longitude: 126.97441248152484,
-                },
-            ].map((match, idx) => ({
-                ...match,
-                color: idx % 2 === 0 ? 'bg-white' : 'bg-point-50',
-            })),
-        })),
+            matchList: state.matchList.filter(
+                (otherUser) => otherUser.id !== otherId,
+            ),
+        }));
+    },
+    fetchMatchList: async (userId) => {
+        const { curPage, totalPages } = get();
+
+        if (curPage > totalPages) {
+            set(() => ({ isLastPage: true }));
+            return;
+        }
+
+        const { ok, data, error, status } = await getMatchList({
+            userId,
+            page: curPage,
+        });
+
+        if (!ok) {
+            console.error(error?.status, error?.msg);
+            return;
+        }
+
+        if (status === 204) {
+            set(() => ({ isLastPage: true }));
+            return;
+        }
+
+        const { matchResults, totalPages: newTotalPage } = data.result;
+
+        if (matchResults.length > 0) {
+            set((state) => ({
+                matchList: matchResults.map((match, idx) => ({
+                    ...match,
+                    color: idx % 2 === 0 ? 'bg-white' : 'bg-point-50',
+                })),
+                curPage: state.curPage,
+                totalPages: newTotalPage,
+            }));
+        }
+    },
 }));
 
 export default useMatchStore;
