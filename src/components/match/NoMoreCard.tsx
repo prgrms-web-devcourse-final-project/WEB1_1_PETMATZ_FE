65da949102599cca491406b08916c2fdcf674d75
Kim-HyounSuk <kkoor586@naver.com>
2024-11-29 23:36:13 +0900

    ✨ 매칭 불가 카드 컴포넌트 작성

---
+++ src/components/match/NoMoreCard.tsx
@@ -0,0 +1,36 @@
+import { motion } from 'framer-motion';
+
+// SVG
+import DogLargeIcon from '@/assets/images/dogs/dogLarge.svg?react';
+
+export default function NoMoreCard() {
+    return (
+        <motion.div
+            className="absolute w-[240px] h-[375px] bg-gray-300 shadow-md rounded-xl flex flex-col items-center justify-center gap-[32px] border-1 border-point-100 text-gray-100"
+            initial={{
+                scale: 0.75,
+                y: 65.5,
+                opacity: 1,
+            }}
+            animate={{
+                y: 0,
+                scale: 1,
+                opacity: 1,
+            }}
+            transition={{ duration: 0.5 }}
+        >
+            <div className="flex flex-col items-center justify-center w-[111.5px] gap-[8px]">
+                <span className="text-center text-body-l font-extrabold">
+                    매칭 가능한
+                </span>
+                <span className="text-center text-body-l font-extrabold">
+                    돌봄이를
+                </span>
+                <span className="text-center text-body-l font-extrabold">
+                    찾을 수 없어요.
+                </span>
+            </div>
+            <DogLargeIcon className="w-[111.5px] h-[120px]" />
+        </motion.div>
+    );
+}
