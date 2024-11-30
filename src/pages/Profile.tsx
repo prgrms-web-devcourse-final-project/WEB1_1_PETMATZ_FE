4d5e34efa2c6ee77e296101dc72f5c6c4504fc71
programmerJuni <tjrwns2715@naver.com>
2024-11-30 19:20:42 +0900

    🐛 프로필 페이지 컴포넌트 마이페이지 확인 및 에러 처리

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,12 +1,17 @@
 import { Loading } from '@/components/common';
 import { getProfileInfo } from '@/hooks/api/profile';
+import { useUserStore } from '@/stores';
 import { ProfileApiResponse } from '@/types/user';
 import { useQuery } from '@tanstack/react-query';
-import { useSearchParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 export default function Profile() {
-    let [query] = useSearchParams();
-    const userId = query.get('userId') || '';
+    const { id } = useParams<{ id: string }>();
+    const { user } = useUserStore();
+
+    const userId = id || '';
+    const isMyProfile = id === user?.id;
+
     const { data, isLoading } = useQuery<ProfileApiResponse, Error>({
         queryKey: ['user', userId],
         queryFn: () => getProfileInfo({ userId }),
@@ -16,17 +21,17 @@ export default function Profile() {
         return <Loading />;
     }
 
-    if (!data || data.ok === false || data.error?.status === 400) {
-        return <div>존재하지 않는 사용자입니다.</div>;
-    } else if (!data.ok) {
+    if (!data || !data.data || data.error?.status === 500) {
         return <div>서버 에러</div>;
+    } else if (data.error?.status === 400) {
+        return <div>존재하지 않는 사용자입니다.</div>;
     }
 
     const profileData = data.data;
 
     return (
         <div>
-            <h1>{profileData.nickname}의 프로필</h1>
+            <h1>{profileData?.nickname}의 프로필</h1>
             {/* 여기에 더 많은 프로필 정보를 표시할 수 있습니다 */}
         </div>
     );
