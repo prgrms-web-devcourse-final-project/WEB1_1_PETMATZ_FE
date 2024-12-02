8759e040f333a0097e7aeed545a71665ed78d3d9
programmerJuni <tjrwns2715@naver.com>
2024-12-02 14:09:16 +0900

    🦺 비밀번호 최대 글자수 지정

--- src/hooks/useSignupForm.ts
+++ src/hooks/useSignupForm.ts
@@ -201,6 +201,10 @@ export default function useSignupForm() {
             value: 8,
             message: '비밀번호는 최소 8자 이상이어야 합니다!',
         },
+        maxLength: {
+            value: 20,
+            message: '비밀번호는 최대 20자 이하이어야 합니다!',
+        },
         pattern: {
             value: /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/,
             message: '비밀번호는 영어, 숫자, 특수문자를 포함해야 합니다!',
