82ff12aa66bbeb339938c337d835026c07881139
programmerJuni <tjrwns2715@naver.com>
2024-12-04 00:16:30 +0900

    💄 회원 탈퇴 완료 페이지 추가

--- src/components/delete-account/Success.tsx
+++ src/components/delete-account/Success.tsx
@@ -1,3 +1,22 @@
+import Logo from '@/assets/images/header/logo.svg?react';
+import CryingDog from '@/assets/images/delete-account/cryingDog.svg?react';
+
 export default function Success() {
-    return <div>완료</div>;
+    return (
+        <div className="h-screen bg-white flex flex-col justify-between overflow-hidden">
+            <header className="h-14 w-full flex items-center justify-center">
+                <Logo />
+            </header>
+            <section className="bg-white flex-1 flex flex-col justify-center items-center mb-16">
+                <CryingDog className="mb-8 z-10" />
+                <p className="text-body-s font-semibold text-gray-400 mb-2">
+                    회원 탈퇴 완료
+                </p>
+                <div className="text-black text-body-xl font-extrabold text-center">
+                    <p>꼭 다시 볼 수 있기를</p>
+                    <p>바래요 멍멍!</p>
+                </div>
+            </section>
+        </div>
+    );
 }
