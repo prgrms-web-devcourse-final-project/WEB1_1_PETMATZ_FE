15cc22a8752f954ca38bb4a3ff519548d3cd8ee1
programmerJuni <tjrwns2715@naver.com>
2024-12-03 23:44:23 +0900

    ✨ 회원 탈퇴 페이지 커스텀 훅 작성

---
+++ src/hooks/useDeleteAccountForm.ts
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useCustomToast } from '@/components/common';
+
+/**
+ * Delete account form input type
+ */
+interface DeleteAccountInputs {
+    /** User's password */
+    password: string;
+}
+
+/**
+ * Custom hook for handling Delete account form logic
+ * @returns {Object} Form methods and handlers
+ */
+export default function useDeleteAccountForm() {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors, isValid },
+    } = useForm<DeleteAccountInputs>({
+        mode: 'onChange',
+    });
+    const [success, setSuccess] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const { showToast, isToastActive } = useCustomToast();
+
+    /**
+     * Handles form submission
+     */
+    const onSubmit = async (data: DeleteAccountInputs) => {
+        // api 로직 추가
+    };
+
+    /**
+     * Password validation
+     */
+    const passwordValidation = {
+        required: '비밀번호는 필수입니다!',
+        minLength: {
+            value: 8,
+            message: '비밀번호는 최소 8자 이상이어야 합니다!',
+        },
+        maxLength: {
+            value: 20,
+            message: '비밀번호는 최대 20자 이하이어야 합니다!',
+        },
+        pattern: {
+            value: /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/,
+            message: '비밀번호는 영어, 숫자, 특수문자를 포함해야 합니다!',
+        },
+    };
+
+    return {
+        passwordValidation,
+        register,
+        handleSubmit,
+        watch,
+        errors,
+        onSubmit,
+        isValid,
+        success,
+        loading,
+    };
+}
