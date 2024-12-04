d278d0c266d293180f7845fdae35ef1dfd2fff42
programmerJuni <tjrwns2715@naver.com>
2024-12-04 20:55:12 +0900

    💄 프로필 페이지 아코디언 버튼 애니메이션

--- src/components/profile/DogList.tsx
+++ src/components/profile/DogList.tsx
@@ -28,36 +28,40 @@ export default function DogList({
     }, []);
 
     return (
-        <article className="flex flex-col gap-4">
+        <>
             <div
                 onClick={handleShowDogsBtn}
                 className="flex justify-between px-6 py-[12.5px] text-body-l font-semibold text-point-900 border-1 border-gray-200 rounded-lg shadow-md cursor-pointer"
             >
                 <span>등록된 멍멍이 정보</span>
-                <Arrow className="text-gray-400" />
+                <Arrow
+                    className={`text-gray-400 ${showMenu ? 'animate-rotate-180' : 'animate-rotate-360'}`}
+                />
             </div>
-            {showMenu &&
-                dogsData.data?.map((dog, index) => (
-                    <DogCard
-                        key={index}
-                        id={dog.id}
-                        dogNm={dog.dogNm}
-                        sexNm={dog.sexNm}
-                        kindNm={dog.kindNm}
-                        neuterYn={dog.neuterYn}
-                        profileImg={dog.profileImg}
-                        age={dog.age}
-                        temperament={dog.temperament}
-                        size={dog.size}
-                        comment={true}
-                        edit={isMyProfile}
-                    />
-                ))}
-            {showMenu && isMyProfile && (
-                <button onClick={handleAddDogBtn} className="btn-solid">
-                    + 멍멍이 등록하기
-                </button>
-            )}
-        </article>
+            <article className="flex flex-col gap-4">
+                {showMenu &&
+                    dogsData.data?.map((dog, index) => (
+                        <DogCard
+                            key={index}
+                            id={dog.id}
+                            dogNm={dog.dogNm}
+                            sexNm={dog.sexNm}
+                            kindNm={dog.kindNm}
+                            neuterYn={dog.neuterYn}
+                            profileImg={dog.profileImg}
+                            age={dog.age}
+                            temperament={dog.temperament}
+                            size={dog.size}
+                            comment={true}
+                            edit={isMyProfile}
+                        />
+                    ))}
+                {showMenu && isMyProfile && (
+                    <button onClick={handleAddDogBtn} className="btn-solid">
+                        + 멍멍이 등록하기
+                    </button>
+                )}
+            </article>
+        </>
     );
 }
