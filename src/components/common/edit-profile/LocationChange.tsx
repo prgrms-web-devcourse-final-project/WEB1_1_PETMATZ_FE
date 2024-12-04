3eddfefd465e72a25810106db96fa8f3f7d53469
ysmuei <108169494+ysmuei@users.noreply.github.com>
2024-12-04 18:52:21 +0900

    💄 로딩 화면 추가

--- src/components/common/edit-profile/LocationChange.tsx
+++ src/components/common/edit-profile/LocationChange.tsx
@@ -1,11 +1,20 @@
-import { postLocation } from '@/hooks/api/location'; // postLocation API 호출
+import { useState } from 'react';
+import { postLocation } from '@/hooks/api/location';
 import { useUserStore } from '@/stores';
 import { useLocation } from '@/hooks/useLocation';
-import { useCustomToast } from '../CustomToast';
+import LoadingSpiner from '@/assets/images/profile/loadingSpiner.svg?react';
 
-export default function LocationChange() {
+interface LocationChangeProps {
+    onLocationChange: () => void; // 위치 변경 후 실행될 콜백
+}
+
+export default function LocationChange({
+    onLocationChange,
+}: LocationChangeProps) {
     const { user } = useUserStore(); // Zustand store에서 사용자 데이터 가져오기
     const { data } = useLocation(); // 현재 위치 정보 가져오기
+
+    const [isLoading, setIsLoading] = useState(false); // 로딩 상태 관리
 
     const handleLocationChange = async () => {
         if (!data || data.latitude === null || data.longitude === null) {
@@ -13,32 +22,36 @@ export default function LocationChange() {
             return;
         }
 
+        setIsLoading(true); // 로딩 시작
         try {
             const updatedLocation = await postLocation(data); // 위치 업데이트 API 호출
-            console.log(updatedLocation);
-            console.log('유저정보', user);
-            useCustomToast;
-            alert('위치가 성공적으로 업데이트되었습니다.');
+            onLocationChange();
         } catch (error) {
             console.error('위치 업데이트 실패:', error);
             alert('위치 업데이트에 실패했습니다.');
+        } finally {
+            setIsLoading(false); // 로딩 종료
         }
     };
 
     return (
         <div className="mb-[18px]">
             <p className="text-label-m font-normal text-gray-500 mb-2">지역</p>
-            <div className="flex gap-2">
+            <div className="flex gap-2 items-center">
                 {/* 사용자 지역 정보 표시 */}
-                <p className="rounded-lg text-label-l font-normal">
+                <p className="rounded-lg text-label-l font-semibold py-1">
                     {user?.region || '지역 정보 없음'}
                 </p>
-                <button
-                    onClick={handleLocationChange} // 위치 변경 버튼 클릭 핸들러
-                    className="text-gray-400 text-body-s font-normal"
-                >
-                    위치 변경
-                </button>
+                {isLoading ? (
+                    <LoadingSpiner className="w-6 h-6 text-purple-900 animate-spin" />
+                ) : (
+                    <button
+                        onClick={handleLocationChange} // 위치 변경 버튼 클릭 핸들러
+                        className="text-gray-400 text-label-m font-normal"
+                    >
+                        위치 변경
+                    </button>
+                )}
             </div>
         </div>
     );
