4f447bae3a3a6923400f2ef1583d7f8075674242
Kim-HyounSuk <kkoor586@naver.com>
2024-12-07 07:51:25 +0900

    ✨ 사용자 팝업 메뉴 추가

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,10 +1,23 @@
 import Logo from '@/assets/images/header/logo.svg?react';
 import NotificationIcon from '@/assets/images/header/notification.svg?react';
 import UserIcon from '@/assets/images/header/user.svg?react';
-import { useTitleStore } from '@/stores';
+import { Dropdown } from '@/components/common';
+import { useTitleStore, useUserStore } from '@/stores';
+import { useFadeNavigate } from '@/hooks';
+
+// SVG
+import ExitIcon from '@/assets/images/header/exit.svg?react';
+import ProfileIcon from '@/assets/images/header/profile.svg?react';
+import HeartIcon from '@/assets/images/profile/heart.svg?react';
 
 export default function Header() {
     const { title } = useTitleStore();
+    const { user } = useUserStore();
+    const navigate = useFadeNavigate();
+
+    const handleClickLogout = () => {
+        // 로그아웃 로직
+    };
 
     return (
         <header className="flex items-center justify-between w-full p-4 text-gray-900 bg-white">
@@ -14,7 +27,31 @@ export default function Header() {
             </h1>
             <div className="flex items-center justify-between w-[60px] h-6">
                 <NotificationIcon className="w-6 h-6 cursor-pointer" />
-                <UserIcon className="w-6 h-6 cursor-pointer" />
+                <Dropdown
+                    icon={<UserIcon className="w-6 h-6 cursor-pointer" />}
+                >
+                    <li
+                        className="flex items-center py-[10px] px-[14px] gap-[8px] hover:bg-point-50 active:bg-point-50 rounded-t-lg"
+                        onClick={() => navigate(`/${user?.id}`)}
+                    >
+                        <ProfileIcon className="w-[18px] h-[18px]" />
+                        <span>내 정보</span>
+                    </li>
+                    <li
+                        className="flex items-center py-[10px] px-[14px] gap-[8px] hover:bg-point-50 active:bg-point-50"
+                        onClick={() => navigate('/like')}
+                    >
+                        <HeartIcon className="w-[18px] h-[18px]" />
+                        <span>찜 목록</span>
+                    </li>
+                    <li
+                        className="flex items-center py-[10px] px-[14px] gap-[8px] text-warning-400 hover:bg-point-50 active:bg-point-50 rounded-b-lg"
+                        onClick={handleClickLogout}
+                    >
+                        <ExitIcon className="w-[18px] h-[18px]" />
+                        <span>로그아웃</span>
+                    </li>
+                </Dropdown>
             </div>
         </header>
     );
