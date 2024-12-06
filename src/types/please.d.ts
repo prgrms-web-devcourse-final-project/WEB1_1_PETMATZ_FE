7c2d9d4b262839751751de8d43822b5a1a6bcb56
dalsu0222 <dalsu0222@sju.ac.kr>
2024-12-06 11:32:09 +0900

    🏷️ 부탁 상세조회 응답 type 생성

--- src/types/please.d.ts
+++ src/types/please.d.ts
@@ -27,4 +27,41 @@ interface CreatePleaseApiResponse extends BaseApiResponse {
     };
 }
 
-export type { FormData, CreatePleaseApiRequest, CreatePleaseApiResponse };
+// 반려견 정보 타입
+interface PetInfo {
+    petName: string;
+    breed: string;
+    age: number;
+    gender: 'MALE' | 'FEMALE';
+    neuterYn: string; // '중성' | '미중성',
+    temperament: string;
+}
+
+// 미션 요청 정보 타입
+interface PetMissionAskInfo {
+    id: number | null;
+    comment: string | null;
+    ask: string;
+    imgURL: string | null;
+}
+
+interface PetMissionInfoResponse extends BaseApiResponse {
+    data: {
+        result: {
+            id: number;
+            careName: string;
+            receiverName: string;
+            receiverStart: string;
+            receiverEnd: string;
+            petMissionPetInfos: PetInfo[];
+            petMissionAskInfos: PetMissionAskInfo[];
+        };
+    };
+}
+
+export type {
+    FormData,
+    CreatePleaseApiRequest,
+    CreatePleaseApiResponse,
+    PetMissionInfoResponse,
+};
