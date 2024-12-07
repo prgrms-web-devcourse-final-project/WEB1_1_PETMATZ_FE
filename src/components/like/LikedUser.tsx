d07a134a8d98a4eadceb83472ef693f2bf0181d0
Pa55er <tjrwns2715@naver.com>
2024-12-07 20:56:37 +0900

    ✨ 찜목록에서 프로필 페이지로 이동 navigate 추가

--- src/components/like/LikedUser.tsx
+++ src/components/like/LikedUser.tsx
@@ -1,5 +1,7 @@
 import Heart from '@/assets/images/profile/heart.svg?react';
+import { useFadeNavigate } from '@/hooks';
 import type { LikedUser } from '@/types/user';
+import { useCallback } from 'react';
 
 export default function LikedUser({
     myId,
@@ -9,9 +11,18 @@ export default function LikedUser({
     careAvailable,
     preferredSizes,
 }: LikedUser) {
+    const navigate = useFadeNavigate();
+
+    const handleProfileBtn = useCallback(() => {
+        navigate(`/profile/${heartedId}`);
+    }, []);
+
     return (
         <li className="w-full max-w-[600px] mx-auto px-6 py-[10px] border-b-2 border-gray-200 flex items-center gap-6">
-            <div className="relative w-[75px] h-[75px] cursor-pointer">
+            <div
+                onClick={handleProfileBtn}
+                className="relative w-[75px] h-[75px] cursor-pointer"
+            >
                 <img
                     src={profileImg}
                     alt="프로필 이미지"
@@ -20,7 +31,9 @@ export default function LikedUser({
             </div>
             <section className="flex-1 flex flex-col justify-around gap-[10px]">
                 <h2 className="text-gray-900 text-body-l font-extrabold">
-                    <span className="cursor-pointer">{nickname}</span>
+                    <span onClick={handleProfileBtn} className="cursor-pointer">
+                        {nickname}
+                    </span>
                 </h2>
                 <div className="flex flex-wrap gap-[10px] text-label-m text-point-500 font-extrabold">
                     <span>#돌봄 {!careAvailable && '불'}가능</span>
