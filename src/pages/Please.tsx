554dfeaa269c6c3bfc9583f8cf40abbf91b22f68
Kim-HyounSuk <kkoor586@naver.com>
2024-12-08 02:12:50 +0900

    ✨ 부탁 리스트 데이터 페칭 테스트

--- src/pages/Please.tsx
+++ src/pages/Please.tsx
@@ -1,3 +1,29 @@
+import { TabMenu } from '@/components/common';
+import { pleaseTab } from '@/constants/please';
+import { useFetchPleaseList } from '@/hooks/please';
+import { useTitleStore } from '@/stores';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export default function Please() {
-    return <div>부탁해 멍멍 페이지</div>;
+    const navigate = useNavigate();
+    const { setTitle } = useTitleStore();
+    const [activeTab, setActiveTab] = useState('care');
+    const { data, isLoading } = useFetchPleaseList();
+
+    useEffect(() => {
+        setTitle('부탁해 멍멍');
+    }, []);
+
+    console.log(data);
+
+    return (
+        <div className="h-full bg-gray-100 overflow-y-auto">
+            <TabMenu
+                tabs={pleaseTab}
+                activeTab={activeTab}
+                onTabChange={setActiveTab}
+            />
+        </div>
+    );
 }
