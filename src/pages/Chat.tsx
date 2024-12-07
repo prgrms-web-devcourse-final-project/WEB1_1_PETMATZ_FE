34efe5af78a20eafea0b7ab23cfd92341f9b3225
Kim-HyounSuk <kkoor586@naver.com>
2024-12-07 15:50:29 +0900

    ♻️ 채팅 리스트 페이지 로딩화면 추가

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,17 +1,24 @@
 import { ChatRoom, NonChatRoom } from '@/components/chat';
+import { Loading } from '@/components/common';
 import { useChatStore, useTitleStore, useUserStore } from '@/stores';
 import { useEffect, useState } from 'react';
 
 export default function Chat() {
     const [swipedRoom, setSwipedRoom] = useState<number | null>(null);
-
     const { setTitle } = useTitleStore();
     const { chatRoomList, fetchChatRoomList, unSubFromChatRoomList } =
         useChatStore();
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
-        fetchChatRoomList();
+        const dataFetching = async () => {
+            setIsLoading(true);
+            await fetchChatRoomList();
+            setIsLoading(false);
+        };
+
         setTitle('채팅');
+        dataFetching();
 
         return () => {
             unSubFromChatRoomList();
@@ -21,7 +28,9 @@ export default function Chat() {
     return (
         <main className="flex flex-col h-full overflow-hidden bg-gray-100">
             <div className="flex flex-col h-full overflow-y-auto">
-                {chatRoomList.length > 0 ? (
+                {isLoading ? (
+                    <Loading />
+                ) : chatRoomList.length > 0 ? (
                     chatRoomList.map((chatRoom) => (
                         <ChatRoom
                             key={chatRoom.chatRoomId}
