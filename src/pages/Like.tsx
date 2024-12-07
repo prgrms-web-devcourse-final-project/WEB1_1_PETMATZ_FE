c60915ffeb8ed91a5d113f2991e04db5579b5f30
Pa55er <tjrwns2715@naver.com>
2024-12-07 18:27:57 +0900

    💄 찜한 돌봄이 리스트 페이지 퍼블리싱

--- src/pages/Like.tsx
+++ src/pages/Like.tsx
@@ -1,3 +1,91 @@
+import { useFadeNavigate } from '@/hooks';
+import { useCallback } from 'react';
+import Back from '@/assets/images/header/back.svg?react';
+import Heart from '@/assets/images/profile/heart.svg?react';
+
 export default function Like() {
-    return <div>찜목록</div>;
+    const navigate = useFadeNavigate();
+
+    const handleBackBtn = useCallback(() => {
+        navigate(-1);
+    }, []);
+
+    return (
+        <div className="h-screen bg-gray-100 flex flex-col overflow-hidden">
+            <header className="bg-white h-14 w-full flex items-center justify-center">
+                <Back
+                    onClick={handleBackBtn}
+                    className="absolute left-[26px] cursor-pointer"
+                />
+                <h1 className="text-point-900 text-body-l font-extrabold">
+                    찜한 돌봄이
+                </h1>
+            </header>
+            <ul className="bg-white flex-1 flex flex-col justify-start overflow-y-auto pb-2.5">
+                <li className="w-full max-w-[600px] mx-auto px-6 py-[10px] border-b-2 border-gray-200 flex items-center gap-6">
+                    <div className="relative w-[75px] h-[75px] cursor-pointer">
+                        <img
+                            src="/src/assets/images/profile/profile1.svg"
+                            alt="프로필 이미지"
+                            className="object-cover w-full h-full rounded-full border-1 border-gray-200"
+                        />
+                    </div>
+                    <section className="flex-1 flex flex-col justify-around gap-[10px]">
+                        <h2 className="text-gray-900 text-body-l font-extrabold">
+                            <span className="cursor-pointer">닉네임</span>
+                        </h2>
+                        <div className="flex flex-wrap gap-[10px] text-label-m text-point-500 font-extrabold">
+                            <span>#돌봄 불가능</span>
+                            <span>#소형</span>
+                            <span>#중형</span>
+                            <span>#대형</span>
+                        </div>
+                    </section>
+                    <Heart className="cursor-pointer w-8 h-8 text-warning-200" />
+                </li>
+                <li className="w-full max-w-[600px] mx-auto px-6 py-[10px] border-b-2 border-gray-200 flex items-center gap-6">
+                    <div className="relative w-[75px] h-[75px] cursor-pointer">
+                        <img
+                            src="/src/assets/images/profile/profile1.svg"
+                            alt="프로필 이미지"
+                            className="object-cover w-full h-full rounded-full border-1 border-gray-200"
+                        />
+                    </div>
+                    <section className="flex-1 flex flex-col justify-around gap-[10px]">
+                        <h2 className="text-gray-900 text-body-l font-extrabold">
+                            <span className="cursor-pointer">닉네임</span>
+                        </h2>
+                        <div className="flex flex-wrap gap-[10px] text-label-m text-point-500 font-extrabold">
+                            <span>#돌봄 불가능</span>
+                            <span>#소형</span>
+                            <span>#중형</span>
+                            <span>#대형</span>
+                        </div>
+                    </section>
+                    <Heart className="cursor-pointer w-8 h-8 text-warning-200" />
+                </li>
+                <li className="w-full max-w-[600px] mx-auto px-6 py-[10px] border-b-2 border-gray-200 flex items-center gap-6">
+                    <div className="relative w-[75px] h-[75px] cursor-pointer">
+                        <img
+                            src="/src/assets/images/profile/profile1.svg"
+                            alt="프로필 이미지"
+                            className="object-cover w-full h-full rounded-full border-1 border-gray-200"
+                        />
+                    </div>
+                    <section className="flex-1 flex flex-col justify-around gap-[10px]">
+                        <h2 className="text-gray-900 text-body-l font-extrabold">
+                            <span className="cursor-pointer">닉네임</span>
+                        </h2>
+                        <div className="flex flex-wrap gap-[10px] text-label-m text-point-500 font-extrabold">
+                            <span>#돌봄 불가능</span>
+                            <span>#소형</span>
+                            <span>#중형</span>
+                            <span>#대형</span>
+                        </div>
+                    </section>
+                    <Heart className="cursor-pointer w-8 h-8 text-warning-200" />
+                </li>
+            </ul>
+        </div>
+    );
 }
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -11,7 +11,7 @@ import Unheart from '@/assets/images/profile/unheart.svg?react';
 import { DogList, Label, Tag } from '@/components/profile';
 import { useCallback, useEffect, useState } from 'react';
 import { useFadeNavigate } from '@/hooks';
-import { createChatRoom } from '@/hooks/api/chat';
+import { createChatRoom } from '@/hooks/api/Chat';
 import { DogsInfoResponse } from '@/types/dogInfo';
 import { fetchDogsInfo } from '@/hooks/api/dogInfo';
 import { getProfileInfo, postLikeProfile } from '@/hooks/api/user';
@@ -113,7 +113,7 @@ export default function Profile() {
                             <img
                                 src={image}
                                 alt="프로필 이미지"
-                                className="object-cover w-full h-full rounded-full border-[1px] border-gray-200"
+                                className="object-cover w-full h-full rounded-full border-2 border-gray-200"
                             />
                         </div>
                         <div className="flex justify-center items-center gap-2">
@@ -172,12 +172,12 @@ export default function Profile() {
                                     {like ? (
                                         <Heart
                                             onClick={handleLikeBtn}
-                                            className="flex-1 cursor-pointer w-8 h-8"
+                                            className="flex-1 cursor-pointer w-8 h-8 text-warning-200"
                                         />
                                     ) : (
                                         <Unheart
                                             onClick={handleLikeBtn}
-                                            className="flex-1 cursor-pointer w-8 h-8"
+                                            className="flex-1 cursor-pointer w-8 h-8 text-warning-200"
                                         />
                                     )}
                                     <span className="text-label-l text-point-800 font-extrabold">
