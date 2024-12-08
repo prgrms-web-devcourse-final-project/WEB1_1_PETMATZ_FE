f85578a2f9d87af655f60a02c632a53cfe5a665d
Kim-HyounSuk <kkoor586@naver.com>
2024-12-08 09:49:05 +0900

    ✨ 부탁해 멍멍 리스트 페이지 작성

--- src/pages/Please.tsx
+++ src/pages/Please.tsx
@@ -1,29 +1,44 @@
-import { TabMenu } from '@/components/common';
+import { Loading } from '@/components/common';
+import { NonPlease, PleaseCard } from '@/components/please';
+import PleaseTabMenu from '@/components/please/PleaseTabMenu';
 import { pleaseTab } from '@/constants/please';
 import { useFetchPleaseList } from '@/hooks/please';
-import { useTitleStore } from '@/stores';
+import { useTitleStore, useUserStore } from '@/stores';
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 export default function Please() {
-    const navigate = useNavigate();
     const { setTitle } = useTitleStore();
-    const [activeTab, setActiveTab] = useState('care');
-    const { data, isLoading } = useFetchPleaseList();
+    const { user } = useUserStore();
+    const [activeTab, setActiveTab] = useState<'DOL' | 'MAL'>('MAL');
+    const { data, isLoading } = useFetchPleaseList({
+        activeTab,
+    });
 
     useEffect(() => {
         setTitle('부탁해 멍멍');
     }, []);
 
-    console.log(data);
-
     return (
-        <div className="h-full bg-gray-100 overflow-y-auto">
-            <TabMenu
+        <div className="h-full bg-gray-100 overflow-y-auto flex flex-col">
+            <PleaseTabMenu
                 tabs={pleaseTab}
                 activeTab={activeTab}
                 onTabChange={setActiveTab}
             />
+            {isLoading && <Loading />}
+            {!isLoading && data && data.length === 0 && <NonPlease />}
+            {!isLoading &&
+                user &&
+                data &&
+                data.length > 0 &&
+                data.map((item) => (
+                    <PleaseCard
+                        key={item.missionId}
+                        item={item}
+                        activeTab={activeTab}
+                        userId={user.id}
+                    />
+                ))}
         </div>
     );
 }
