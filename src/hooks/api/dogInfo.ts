471c5be0e3e1e79eeaec70019ed1db3a0309e0c0
ysmuei <ysmuei@naver.com>
2024-12-08 14:23:21 +0900

    ✨ 멍멍이 프로필 수정 api 연동 및 타입 지정

--- src/hooks/api/dogInfo.ts
+++ src/hooks/api/dogInfo.ts
@@ -1,5 +1,12 @@
 import { http } from './base';
-import { DogInfoResponse, DogsInfoResponse } from '@/types/dogInfo';
+import {
+    DogInfoResponse,
+    DogsInfoResponse,
+    DogProfileRequestBody,
+    DogProfileResponse,
+    DeleteDogResponse,
+    DeleteDogRequest,
+} from '@/types/dogInfo';
 
 // 유저에 등록된 강아지 목록 확인시 요청하는 api
 export const fetchDogInfo = async (id: number): Promise<DogInfoResponse> => {
@@ -9,4 +16,24 @@ export const fetchDogInfo = async (id: number): Promise<DogInfoResponse> => {
 // 유저에 등록된 강아지 목록 확인시 요청하는 api
 export const fetchDogsInfo = async (id: number): Promise<DogsInfoResponse> => {
     return await http.get<DogsInfoResponse>(`/api/sosboard/pets/${id}`);
+};
+
+/**
+ * PUT Edit Dog Profile Information
+ * 강아지 프로필 정보를 수정합니다.
+ */
+export const editDogProfileInfo = async (
+    id: number,
+    data: DogProfileRequestBody,
+): Promise<DogProfileResponse> =>
+    await http.put<DogProfileResponse, DogProfileRequestBody>(
+        `/api/pets/${id}`,
+        data,
+    );
+
+// 강아지 정보 삭제 API 함수
+export const deleteDogInfo = async (id: number): Promise<DeleteDogResponse> => {
+    return await http.delete<DeleteDogResponse, DeleteDogRequest>(
+        `/api/pets/${id}`,
+    );
 };
