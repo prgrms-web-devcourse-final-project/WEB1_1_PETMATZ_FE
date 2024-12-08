6fe9e01af71daff983569ae1d809ef31ff25dcae
Kim-HyounSuk <kkoor586@naver.com>
2024-12-08 09:47:38 +0900

    ✨ 부탁해 멍멍관련 상수 타입 선언

--- src/constants/please/index.ts
+++ src/constants/please/index.ts
@@ -1,4 +1,12 @@
-export const pleaseTab = [
-    { id: 'care', label: '돌봄 리스트' },
-    { id: 'entrusted', label: '맡김 리스트' },
+type TabId = 'DOL' | 'MAL';
+
+export const pleaseTab: { id: TabId; label: string }[] = [
+    { id: 'MAL', label: '돌봄 리스트' },
+    { id: 'DOL', label: '맡김 리스트' },
 ];
+
+export const PROGRESS_TYPE: Record<'BEF' | 'INP' | 'AFT', string> = {
+    BEF: '진행전',
+    INP: '진행중',
+    AFT: '완료',
+};
